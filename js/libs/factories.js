f1a4365ac93ac413c168b3933ca8b191b75163f9
hiraq <hrxwan@gmail.com>
2014-10-04 21:35:31 +0700

    setup angular factory for pusher

--- js/libs/factories.js
+++ js/libs/factories.js
@@ -7,4 +7,13 @@ var factories = angular.module('qiscus.factories', []);
     */
     factories.factory('_', ['$window', function($window) {
         return $window._;
-    }]);    
+    }]);
+
+    /*
+    setup factory for pusher
+    */
+    factories.factory('pusher', function() {
+        return new Pusher('4c20f4052ecd7ffc6b0d', {
+            disableStats: true
+        });
+    });
