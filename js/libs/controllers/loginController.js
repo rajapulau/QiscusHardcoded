abda586098eb66faef07a931a94dde70282b71f8
rajapulau <rajapulau@gmail.com>
2014-10-07 19:14:56 +0700

    form login

---
+++ js/libs/controllers/loginController.js
@@ -0,0 +1,10 @@
+qiscus.controller('loginCtrl',function($rootScope, $injector, $scope, $http, $location){
+        $scope.loginAction = function(){
+            if($scope.email === "hrxoneread@yahoo.com" && $scope.password ==="testing_chrome"){
+                console.log($location);
+                $location.path('popup.html');
+            }else{
+                $scope.notif = "Email & Password Invalid";
+            }
+        }
+});
